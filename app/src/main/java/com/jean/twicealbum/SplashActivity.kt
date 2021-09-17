1432a72e03c505da355c69a97c4641bda97be728
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-09-17 19:12:23 +0900

    화면간에 값을 주고 받는 방법 실습

--- app/src/main/java/com/jean/twicealbum/SplashActivity.kt
+++ app/src/main/java/com/jean/twicealbum/SplashActivity.kt
@@ -1,11 +1,22 @@
 package com.jean.twicealbum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
+//권장되지 않는 어노테이션 무시
+@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //3초 뒤에 메인 액티비티로 이동 스플레쉬는 종료
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
